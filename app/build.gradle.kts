fc3c842f1be5bdc40573ac4dad6e47fb68a6a187
hulksmashjaxon <jaxonhulksmash@gmail.com>
2026-08-14 21:45:04 -0700

    `test:(actions)` testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
     implementation("jakarta.annotation:jakarta.annotation-api:3.0.0")
 }
 
-// Apply a specific Java toolchain to ease working on different environments.
+// Apply a specific Java toolchain to ease working on different environments
 java {   
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
